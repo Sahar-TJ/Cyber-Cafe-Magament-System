94cf469eaa4adc81d2f19a2929b89fdf260f5244
Sahar Tosif Jamal <66439789+Sahar-TJ@users.noreply.github.com>
2022-03-23 13:23:15 +0300

    About Us Page

---
+++ aboutus.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel= "stylesheet" href="assets/css/styleh.css">
+</head>
+<body>
+<div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">Cyber Cafe</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="/ccms/homepage.php"> HOME </a></li>
+                    <li><a href="/ccms/aboutus.php">ABOUT</a></li>
+                    <li><a href="/ccms/homepage.php#service">SERVICES</a></li>
+                    <li><a href="/ccms/homepage.php#gallery">GALLERY</a></li>
+                    <li><a href="#footer">CONTACT US</a></li>
+                </ul>
+            </div>
+
+            <div class="log">
+                
+                <a href="/ccms/signup.php"> <button class="butn">SIGN UP</button></a>
+
+                <a href="/ccms/userlogin.php"> <button class="btn">LOG IN</button></a>
+            </div>
+
+        </div> 
+        <div class="content">
+            <h1><span>About Us</span> <br></h1>
+            <br><br><br>
+            <p class="para">India's first Gaming Cyber Cafe. With more than 1000 branches.<br>
+             Our aim is to provide an innovative service to our users. <br> 
+             They will experience an Environment  </p>
+        </div>
+</div>
+
+
+<div class="team" id="team">
+    <h1>Meet the Team</h1><br>
+
+  <div class="mem">
+    <div class="card">
+      <img src="images/Sahar.jpeg" alt="Sahar" style="width:100%", height="440px">
+      <div class="contain">
+        <h2>Sahar Tosif Jamal</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Final Year BCA Student</p>
+        <p>sahartosif@gmail.com</p><br>
+        <p><button class="button">Contact</button></p><br>
+      </div>
+    </div>
+  </div>
+
+  <div class="mem">
+    <div class="card">
+      <img src="images/archana.jpeg" alt="Archana" style="width:100%", height="440px">
+      <div class="contain">
+        <h2>Archana Kumari</h2>
+        <p class="title">Art Director</p>
+        <p>Final Year BCA Student</p>
+        <p>archanakumari@gmail.com</p><br>
+        <p><button class="button">Contact</button></p><br>
+      </div>
+    </div>
+  </div>
+
+  <div class="mem">
+    <div class="card">
+      <img src="images/simra.jpeg" alt="Simra" style="width:100%", height="440px">
+      <div class="contain">
+        <h2>Simra Naeem</h2>
+        <p class="title">Designer</p>
+        <p>Final Year BCA Student</p>
+        <p>simra@gmail.com</p><br>
+        <p><button class="button">Contact</button></p><br>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="footer" id="footer">
+    <div class="cntn">
+        <div class="rw">
+            <div class="footer-col">
+                <h4>Company </h4>
+                        <a href="/ccms/aboutus.php">About us</a>
+                        <a href="/ccms/homepage.php#service">Services</a>
+                        <a href="/ccms/homepage.php#gallery">Gallery</a>
+                        <a href="/ccms/homepage.php#team">Team</a>                
+            </div>
+
+            <div class="footer-col">
+                <h4>Join Us</h4>        
+                        <a href="/ccms/userlogin.php">Login</a>
+                        <a href="/ccms/signup.php">Sign Up</a> 
+                        <a href="/ccms/index.php">Admin Login</a> 
+            </div>
+
+            <div class="footer-col">
+                <h4>Contact</h4>                
+                        <p>Sam academy, </p>
+                        <p>Makhanlal Chaturvedi University,</p>
+                        <p>Bhopal, Madhya Pradesh</p>
+                        <p>+91 97527 32763</p>
+            </div>
+
+            <div class="footer-col">
+                <h4> Follow Us </h4>
+                <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
